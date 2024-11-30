884e46de4a3036d4636f19d0fefdaeb40be238c8
MJServices <mjservices410@gmail.com>
2024-11-30 19:05:40 +0500

    Errors confusion

--- src/utilities/mangaFetcher.tsx
+++ src/utilities/mangaFetcher.tsx
@@ -1,15 +1,18 @@
 import axios from "axios";
 
 export const fetchMangaList = async () => {
-  const targetUrl = 'https://api.mangadex.org/manga';
-  const response = await axios.get('https://api.mangadex.org/manga', {
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
+  try {
+    const response = await axios.get('https://api.mangadex.org/manga', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    }
+    );
+    const data = response.data;
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.log(error)
   }
-  );
-  const data = response.data;
-  console.log(data)
-  return data;
 };
