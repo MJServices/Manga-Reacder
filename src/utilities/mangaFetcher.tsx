a97d4fd4b3cc03bfe86b7e0ccb6de22cbf1e9749
MJServices <mjservices410@gmail.com>
2024-11-30 18:49:32 +0500

    fix

--- src/utilities/mangaFetcher.tsx
+++ src/utilities/mangaFetcher.tsx
@@ -2,7 +2,14 @@ import axios from "axios";
 
 export const fetchMangaList = async () => {
   const targetUrl = 'https://api.mangadex.org/manga';
-  const response = await axios.get(targetUrl);
+  const response = await axios.get('https://api.mangadex.org/manga', {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+  }
+  );
   const data = response.data;
+  console.log(data)
   return data;
 };
